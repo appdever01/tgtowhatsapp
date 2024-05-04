e32483d6d6946e080ec9730b9c8a93c866993f22
appdever01 <appdever01@gmail.com>
2024-05-04 19:13:28 +0100

    m

--- bot.js
+++ bot.js
@@ -271,7 +271,7 @@ const start = async () => {
             text.includes(errorMessage) ? caption : text
           );
           saveWords(keywords);
-          text = `*${channel}*\n\n${text}`;
+          text = `*${text}`;
           const replyData = type === "text" ? text : { url: mediaUrl };
           if (skipper && text.includes(errorMessage)) {
             client.log(
